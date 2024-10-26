8a0506bb077e7698a760e3b97f15e30c55376ed3
Chris Daley <chebizarro@gmail.com>
2024-10-26 12:05:38 -0700

    added unit tests

--- android/src/test/java/biz/nostr/android/nip55/ExampleUnitTest.java
+++ android/src/test/java/biz/nostr/android/nip55/ExampleUnitTest.java
@@ -4,14 +4,38 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
-public class ExampleUnitTest {
+@RunWith(MockitoJUnitRunner.class)
+public class IntentBuilderTest {
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void testGetPublicKeyIntent() {
+        String packageName = "com.example.signerapp";
+        String permissions = "read,write";
+
+        Intent intent = IntentBuilder.getPublicKeyIntent(packageName, permissions);
+
+        assertEquals(Intent.ACTION_VIEW, intent.getAction());
+        assertEquals("nostrsigner:", intent.getDataString());
+        assertEquals(packageName, intent.getPackage());
+        assertEquals("get_public_key", intent.getStringExtra("type"));
+        assertEquals(permissions, intent.getStringExtra("permissions"));
+    }
+}
+
+@RunWith(MockitoJUnitRunner.class)
+public class ContentProviderTest {
+
+    @Mock
+    ContentResolver mockContentResolver;
+
+    @Mock
+    Cursor mockCursor;
+
+    @Test
+    public void testQueryContentProvider() {
+        // Setup mock behavior
+        when(mockContentResolver.query(any(Uri.class), any(), any(), any(), any()))
+                .thenReturn(mockCursor);
+
     }
 }
